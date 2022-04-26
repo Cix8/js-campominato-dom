128312cdce0b6cb00861e61ea3c4ad1096577cd5
Cix8 <francesco.natal_fn@libero.it>
2022-04-26 16:59:20 +0200

    refactoring in js, add some functions to make the code cleaner

--- js/script.js
+++ js/script.js
@@ -8,87 +8,102 @@ const numberOfBombs = 16;
 
 let max = 100;
 
+let score = 0;
 let endMessage = '';
 
-startBtn.addEventListener('click',
-    function() {
-        let score = 0;
-        instructions.classList.add('d-none');
-        finalResult.classList.add('d-none');
-        endTitle.innerHTML = '';
-        gridContainer.innerHTML = '';
-        gridContainer.classList.remove('d-none');
-        let gameMode = difficultySelect.value;
-        console.log(gameMode);
-        let classMode = '';
+startBtn.addEventListener('click', mainGame);
 
-        if (gameMode === 'easy') {
-            max = 100;
-            classMode = 'easy-mode';
-        } else if (gameMode === 'hard') {
-            max = 81;
-            classMode = 'hard-mode';
-        } else {
-            max = 49;
-            classMode = 'crazy-mode';
-        }
+//              MAIN-GAME FUNCTION
 
-        const bombArray = [];
+/**
+ * Description: is the function that regulates the game logic and the game screen according to the user's click
+ */
+function mainGame() {
+    let score = 0;
+    instructions.classList.add('d-none');
+    finalResult.classList.add('d-none');
+    endTitle.innerHTML = '';
+    gridContainer.innerHTML = '';
+    gridContainer.classList.remove('d-none');
+    let gameMode = difficultySelect.value;
+    console.log(gameMode);
+    let classMode = '';
 
-        while (bombArray.length < numberOfBombs) {
-            const rndNumber = getRndInteger(1, max);
-            console.log(rndNumber);
-            if (!bombArray.includes(rndNumber)) {
-                bombArray.push(rndNumber);
-            }
-        }
-
-        console.log(bombArray);
-
-        for (let i = 1; i <= max; i++) {
-            const newElement = document.createElement('div');
-            newElement.classList.add('box',classMode);
-            console.log(newElement);
-
-            const newItem = `<span>${i}</span>`;
-            newElement.innerHTML = newItem;
-
-            newElement.addEventListener('click',
-                function() {
-                    if (bombArray.includes(i)) {
-                        this.classList.add('bomb');
-                        endMessage = 'Hai perso! Il tuo punteggio è di: '+score;
-                        finalResult.classList.remove('d-none');
-                        endTitle.classList.remove('text-green');
-                        endTitle.classList.add('text-red');
-                    } else {
-                        this.classList.add('active', 'no-pointer-events');
-                        console.log(this);
-                        score++;
-                    }
-
-                    if (score >= (max - numberOfBombs)) {
-                        endMessage = 'Complimenti, hai raggiunto il punteggio massimo di: '+score+'. Hai vinto!!!';
-                        finalResult.classList.remove('d-none');
-                        endTitle.classList.remove('text-red');
-                        endTitle.classList.add('text-green');
-                    } 
-
-                    endTitle.innerHTML = endMessage;
-                    console.log(score);
-                }
-            )
-
-            gridContainer.append(newElement);
-        }
-
-        console.log(gridContainer);
-
+    if (gameMode === 'easy') {
+        max = 100;
+        classMode = 'easy-mode';
+    } else if (gameMode === 'hard') {
+        max = 81;
+        classMode = 'hard-mode';
+    } else {
+        max = 49;
+        classMode = 'crazy-mode';
     }
-)
 
-//  UTILITY-FUNCTIONS
+    const bombArray = [];
+    generateEnemiesInBox(bombArray, numberOfBombs, max);
+
+    for (let i = 1; i <= max; i++) {
+        const newElement = document.createElement('div');
+        newElement.classList.add('box',classMode);
+        console.log(newElement);
+
+        const newItem = `<span>${i}</span>`;
+        newElement.innerHTML = newItem;
+
+        newElement.addEventListener('click',
+            function() {
+                if (bombArray.includes(i)) {
+                    this.classList.add('bomb');
+                    endMessage = 'Hai perso! Il tuo punteggio è di: '+score;
+                    finalResult.classList.remove('d-none');
+                    endTitle.classList.remove('text-green');
+                    endTitle.classList.add('text-red');
+                } else {
+                    this.classList.add('active', 'no-pointer-events');
+                    console.log(this);
+                    score++;
+                }
+
+                if (score >= (max - numberOfBombs)) {
+                    endMessage = 'Complimenti, hai raggiunto il punteggio massimo di: '+score+'. Hai vinto!!!';
+                    finalResult.classList.remove('d-none');
+                    endTitle.classList.remove('text-red');
+                    endTitle.classList.add('text-green');
+                } 
+
+                endTitle.innerHTML = endMessage;
+                console.log(score);
+            }
+        )
+
+        gridContainer.append(newElement);
+    }
+
+    console.log(gridContainer);
+
+}
+
+//              UTILITY-FUNCTIONS
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+/**
+ * Description: this function builds an array with 'enemies' and their relative coordinates based on the grid-size
+ * @param {Array} enemiesArray -> an empty array where we are going to push the enemies
+ * @param {Number} numberOfEnemies -> the final length of the enemiesArray
+ * @param {Number} maxBoxNumber -> the max numerical value of the coordinates to be assigned to each enemy
+ */
+function generateEnemiesInBox(enemiesArray, numberOfEnemies, maxBoxNumber) {
+    while (enemiesArray.length < numberOfEnemies) {
+        const rndNumber = getRndInteger(1, maxBoxNumber);
+        console.log(rndNumber);
+        if (!enemiesArray.includes(rndNumber)) {
+            enemiesArray.push(rndNumber);
+        }
+    }
+
+    console.log(enemiesArray);
 }
